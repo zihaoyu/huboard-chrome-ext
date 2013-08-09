43d422c90e60133b01fc6d0278bd7dbf9ee76ba7
Zihao Yu <zihao.yu@hbo.com>
2013-08-09 11:24:58 -0400

    add direct links to issues, pulls, wiki

--- js/linkify.js
+++ js/linkify.js
@@ -5,5 +5,14 @@ var github = 'https://github.com';
 var authorLink = github + '/' + author;
 var repoLink = github + '/' + author + '/' + repo;
 
-$('span.author').html('<a href="' + authorLink + '" target="_blank">' + author + '</a>');
-$('span.repo').html('<a href="' + repoLink + '" target="_blank">' + repo + '</a>');
+function makeHyperlink(link, text) {
+	return '<a href="' + link + '" target="_blank">' + text + '</a>';
+}
+
+$('span.author').html(makeHyperlink(authorLink, author));
+
+var repoHtml = makeHyperlink(repoLink, repo) + ' <span class="sub-header">(' +
+	makeHyperlink(repoLink + '/issues', 'issues') + ', ' +
+	makeHyperlink(repoLink + '/pulls', 'pulls') + ', ' +
+	makeHyperlink(repoLink + '/wiki', 'wiki') + ')</span>';
+$('span.repo').html(repoHtml);
